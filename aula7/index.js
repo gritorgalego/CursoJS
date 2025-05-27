58dd690032f0e634647ba20f1646e5ace8e7ad08
gritorgalego <vitorgalego1606@gmail.com>
2025-05-27 20:52:18 -0300

    Constantes

---
+++ aula7/index.js
@@ -0,0 +1,16 @@
+// Não podemos modificar o valor de uma constante (afinal é uma constante)
+// camelCase
+// Não utilize VAR, utilize CONST
+
+const nome = 'Galego';
+console.log(nome);
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+console.log(typeof(primeiroNumero + segundoNumero));
