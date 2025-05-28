059e4953cf1838de8077007e9f1ad44a4b5c4a96
gritorgalego <vitorgalego1606@gmail.com>
2025-05-28 19:17:52 -0300

    Anotação diferença Let e Var

---
+++ aula9/index.js
@@ -0,0 +1,6 @@
+/* Diferenças Let e Var
+
+É possível declarar a mesma variável mais de uma vez com Var, já com let isso não é possível 
+É recomendado não declarar variáveis sem usar let ou const ou var, pq vai criar variável global
+
+*/
