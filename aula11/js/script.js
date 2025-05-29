517666f920a0190673cbc5f8981708145829fde1
gritorgalego <vitorgalego1606@gmail.com>
2025-05-29 20:25:55 -0300

    Trabalhando com alertas, confirm e prompt no navegador

---
+++ aula11/js/script.js
@@ -0,0 +1,16 @@
+// alert retorna undefined | confirm retorna boolean | prompt retorna string
+
+alert('Seize the day!');
+
+const num1 = prompt('Digite um número:');
+const num2 = prompt('Digite outro número:');
+const soma = Number(num1) + Number(num2);
+alert(`O resultado da soma dos números inseridos é igual a: ${soma}`);
+
+
+// confirm('Are you ready to learn JavaScript?');
+// prompt('What is your name?');
+// const confirma = confirm('Are you sure you want to continue?');
+// console.log(confirma);
+// const nome = prompt('What is your name?');
+// console.log(nome);
