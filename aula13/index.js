0e25a5e003bb22745fd893842dea8ed674441595
gritorgalego <vitorgalego1606@gmail.com>
2025-05-29 22:03:16 -0300

    Trabalhando com Strings

---
+++ aula13/index.js
@@ -0,0 +1,15 @@
+//                01234
+
+let testString = 'testando a divisão de string utiliando o método split';
+
+//console.log(testString.charAt(3)); // t
+// console.log(testString.concat(': adicionando mais texto usando o concat'));
+// console.log(testString.indexOf('te'));
+// console.log(testString.length);
+// console.log(testString.slice(1,4));
+// console.log(testString + ': adicionando mais texto usando o operador +');
+// console.log(`${testString}: adicionando mais texto usando o template string`);
+// console.log(testString.slice(-2)) = console.log(testString.slice(3));
+// console.log(testString.split('a'));
+// console.log(testString.toUpperCase());
+// console.log(testString.toLowerCase());
