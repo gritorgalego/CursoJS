69720fe803c0bdb9b885fd1076650f65ce865203
gritorgalego <vitorgalego1606@gmail.com>
2025-06-02 19:03:37 -0300

    Básico de arrays

---
+++ aula16/index.js
@@ -0,0 +1,16 @@
+//               0        1        2           3     4 
+const alunos = ['Vitor', 'Brian', 'Denis']; // João, Maria
+
+console.log(alunos instanceof Array); // true
+
+// alunos.push('João');
+// alunos.push('Maria');
+// console.log(typeof alunos); // object
+// console.log(alunos.slice(0,-3));
+// alunos.shift(); // Remove o primeiro elemento do array
+// const removido = alunos.pop(); // Remove o último elemento do array
+// console.log(removido);
+// alunos.push('Sade'); // Adiciona no final do array
+// alunos.unshift('Joelson'); // Adiciona no início do array
+// alunos[1] = 'José';
+// alunos[3] = 'Jean';
