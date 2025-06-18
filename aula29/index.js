817013271133332ef09a1d89b19a34b3860f2de3
gritorgalego <vitorgalego1606@gmail.com>
2025-06-18 20:37:49 -0300

    Diferenças var e let

---
+++ aula29/index.js
@@ -0,0 +1,32 @@
+// const verdadeira = true;
+
+// //Let tem escopo de bloco {... bloco}
+// //Var só tem escopo de função
+
+// let nome = "Leorio"; //criando
+// var nome2 = "Kurapika"; //criando
+
+// if (verdadeira) {
+//   var nome2 = "Alluka"; //redeclarando
+//   let nome = "Killua"; //criando
+
+//   if (verdadeira) {
+//     var nome2 = "Ging"; //redeclarando
+//     let nome = "Gon"; //criando
+//   }
+// }
+ 
+// console.log(nome, nome2);
+
+// var sobrenome = 'Galego';
+
+// function sayHello(){
+//     console.log(sobrenome);
+// }
+// sayHello();
+
+// var sobrenome = "Galego";
+// console.log(sobrenome);
+
+// console.log(sobrenome);
+// let sobrenome = "Galego";
