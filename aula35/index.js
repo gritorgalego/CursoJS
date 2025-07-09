d17a1f76160dfad7ca972932859ca54e63f55f3f
gritorgalego <vitorgalego1606@gmail.com>
2025-07-09 20:34:25 -0300

    For of e forEach (bônus)

---
+++ aula35/index.js
@@ -0,0 +1,19 @@
+// For of ->
+
+const nomes = ["Brian", "Stewie", "Peter"];
+
+// for (let i = 0; i < nomes.length; i++){
+//     console.log(nomes[i]);
+// }
+
+// for (let i in nomes){
+//     console.log(nomes[i]);
+// }
+
+// for (let valor of nomes){
+//     console.log(valor);
+// }
+
+nomes.forEach(function (valor, indice, array) {
+  console.log(valor, indice, array);
+});
