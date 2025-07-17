0cf8d66401e7c4ac536137bfcdf5e776d1b4e94c
gritorgalego <vitorgalego1606@gmail.com>
2025-07-17 18:16:30 -0300

    Exercício lógica 1

---
+++ aula39/index.js
@@ -0,0 +1,11 @@
+// Escreva uma função que receba 2 números e retorne o maior deles
+
+// function maiorNumero(num1, num2) {
+//   if (num1 > num2) return num1;
+//   return num2;
+//   //return x > y ? x : y;  Outra forma de fazer
+// }
+const maiorNumero = (x, y) => x > y ? x : y;
+
+maior = maiorNumero(16, 6);
+console.log(maior);
