c61b4c754068256165ab8812d4d23384dba071d7
gritorgalego <vitorgalego1606@gmail.com>
2025-07-22 15:22:47 -0300

    Exercício lógica 2

---
+++ aula40/index.js
@@ -0,0 +1,8 @@
+// Escreva uma função chamada ePaisagem que 
+// recebe dois parâmetros, largura e altura
+// de uma imagem (numer)
+// Retorne true se a imagem estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura; //Usando arrow function
+
+console.log(ePaisagem(1920, 1080));
