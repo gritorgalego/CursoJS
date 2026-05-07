d01abc2c9ee836e1181c31ca11e49c08001d9ebd
gritorgalego <vitorgalego1606@gmail.com>
2026-05-07 15:51:13 -0300

    Criando pasta modelo para o webpack

---
+++ modelo-webpack/public/assets/js/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Logando o index.js");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ modelo-webpack/src/index.js
@@ -0,0 +1,1 @@
+console.log("Logando o index.js");
