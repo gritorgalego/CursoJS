eebce852d41903321c8f96b3fd1b11e8af5ba961
gritorgalego <vitorgalego1606@gmail.com>
2026-05-19 16:09:09 -0300

    Garantindo compatibilidade com Navegadores mais antigos usando Core.js e regenerator-runtime.

---
+++ aula96/src/main.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime/runtime';  // Esses dois "imports" garantem que funcione em navegadores mais antigos
+import 'core-js/stable';
+
+import executa from './modules/promisses';
+
+import './assets/css/style.css';
+
+executa();
